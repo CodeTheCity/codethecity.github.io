4e6d0e8f0f5a2376b51fcfd54ae46d149890e031
Andrew Sage <andrew@asae.co.uk>
2020-03-31 18:08:57 +0100

    Data import process refactored

--- create_database.py
+++ create_database.py
@@ -13,19 +13,23 @@ if __name__ == '__main__':
 
 		df=pd.read_excel(google_sheet_url, dtype={6:'string', 7:'string',12:'string', 13:'string'}, parse_dates=[0])
 
-		sheet_names = ['1914', '1915', '1916', '1917', '1918', '1919', '1920']
-		for items in sheet_names[1:]:
-			dfnew=pd.read_excel(google_sheet_url, sheet_name=items, dtype={6:'string', 7:'string',12:'string', 13:'string'}, parse_dates=[0])
+		for year in range(1915, 1921):
+			dfnew=pd.read_excel(google_sheet_url, sheet_name=str(year), dtype={6:'string', 7:'string',12:'string', 13:'string'}, parse_dates=[0])
 			df = pd.concat([df, dfnew])
 
-		df = df.rename(columns = {'Date of Arrival (dd-mmm-yyyy)':'date', 'Number':'number', 'Ship\'s Name':'vessel', 'Of What Port':'registered_port', 'Master':'master', 'Registered Tonnage':'registerd_tonnage','Port From Whence':'from_port','Cargo':'cargo', 'Wind and Weather':'weather','Other Notes':'notes', 'Transcriber Notes':'transcriber_notes', 'Transcribed by':'transcriber', 'Checked by':'checker', 'Queries':'transcriber_queries'})
+		df = df.rename(columns = {'Date of Arrival (dd-mmm-yyyy)':'date', 'Number':'number', 'Ship\'s Name':'vessel', 'Of What Port':'registered_port', 'Master':'master', 'Registered Tonnage':'registered_tonnage','Port From Whence':'from_port','Cargo':'cargo', 'Wind and Weather':'weather','Other Notes':'notes', 'Transcriber Notes':'transcriber_notes', 'Transcribed by':'transcriber', 'Checked by':'checker', 'Queries':'transcriber_queries'})
 
 		# Fix missing data issues
 		df['cargo'] = df['cargo'].fillna("?")
 		df['registered_port'] = df['registered_port'].fillna('?')
 		df['from_port'] = df['from_port'].fillna('?')
 		df["vessel"] = df["vessel"].fillna('?')
-		df['registerd_tonnage'] = df[['registerd_tonnage']].apply(pd.to_numeric, errors='coerce')
+		df["weather"] = df["weather"].fillna('?')
+		df["notes"] = df["notes"].fillna('?')
+		df["transcriber_notes"] = df["transcriber_notes"].fillna('?')
+		df["transcriber"] = df["transcriber"].fillna('?')
+		df["checker"] = df["checker"].fillna('?')
+		df["transcriber_queries"] = df["transcriber_queries"].fillna('?')
 
 		# Write to database
 		con = db.create_connection()
