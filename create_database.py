9c29e6e219ed8855bf19177dc0103c7ce8dcdc6b
Andrew Sage <andrew@asae.co.uk>
2020-08-02 11:32:53 +0100

    #12 Adjustments data set imported and merged to include Fishing Port Registration Number.

--- create_database.py
+++ create_database.py
@@ -4,9 +4,11 @@ import pandas as pd
 import database_driver
 import datetime
 from pathlib import Path  # <= Added Line
+import process_adjustments as pa
 
 dbname = 'harbour.db'
 db = database_driver.database(dbname)
+
  
 if __name__ == '__main__':
 
@@ -79,7 +81,7 @@ if __name__ == '__main__':
 			# This overwrites the existing content and imports the current year
 			# This works even if there is a 0916 year in the date column
 			con = db.create_connection()
-			dfnew.to_sql('arrivals', con, if_exists='replace', index = False)
+			dfnew.to_sql('arrivals_temp', con, if_exists='replace', index = False)
 			con.close()
 
 			print('Imported {} into database'.format(year))
@@ -92,7 +94,7 @@ if __name__ == '__main__':
 			# Write to database
 			# This was added to check each year and would be removed once everything works
 			con = db.create_connection()
-			df.to_sql('arrivals', con, if_exists='replace', index = False)
+			df.to_sql('arrivals_temp', con, if_exists='replace', index = False)
 			con.close()
 
 		df.columns = ['uuid', 'date','number','vessel','registered_port','master','registered_tonnage','from_port','cargo_transcribed','weather','notes', 'transcriber_notes', 'transcriber', 'checker', 'transcriber_queries']
@@ -177,10 +179,13 @@ if __name__ == '__main__':
 
 		# Write to database
 		con = db.create_connection()
-		df.to_sql('arrivals', con, if_exists='replace', index = False)
+		df.to_sql('arrivals_temp', con, if_exists='replace', index = False)
 		con.close()
 
-		rows = db.select('SELECT COUNT(date) FROM arrivals')
+		rows = db.select('SELECT COUNT(date) FROM arrivals_temp')
 
 		print('{} imported'.format(rows[0][0]))
 		db.execute_query("INSERT INTO imports values((?), (?))", (datetime.datetime.now(), rows[0][0]))
+
+		processor = pa.ProcessAdjustments()
+		processor.process()
---
+++ process_adjustments.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import numpy as np
+import pandas as pd
+import database_driver
+import datetime
+from pathlib import Path  # <= Added Line
+
+dbname = 'harbour.db'
+
+class ProcessAdjustments:
+	def __init__(self):
+		self.db = database_driver.database(dbname)
+
+	def process(self):
+
+		# Import transcribed records
+		google_sheet_url = 'https://docs.google.com/spreadsheets/d/1S4Kfl0gskYiWXtinLfh2pNMiFbS7-8qgRWTGY11XD6E/export#gid=0?format=csv'
+
+		df = pd.DataFrame()
+		
+		dfAdjustments = pd.read_excel(google_sheet_url, header=0, parse_dates=[1], na_values=['(blank)', '(Blank)','Blank','blank', '-',''])
+
+		# Just to see what has been imported
+		#print(dfAdjustments)
+
+		con = self.db.create_connection()
+		dfOriginal = pd.read_sql_query('SELECT * FROM arrivals_temp ORDER BY date', con, parse_dates=['date'])
+		con.close()
+
+		#print(dfOriginal)
+
+		#print(dfOriginal.dtypes)
+		#print(dfAdjustments.dtypes)
+
+		dfAdjusted = dfOriginal.merge(dfAdjustments[['uuid', 'fishing_port_registration_numbers']], on='uuid')
+
+		dfAdjusted = dfAdjusted.rename(columns={'fishing_port_registration_numbers':'fishing_port_registation_number'})
+
+		print(dfAdjusted)
+		print(dfAdjusted.dtypes)
+
+		# Write to database
+		con = self.db.create_connection()
+		dfAdjusted.to_sql('arrivals', con, if_exists='replace', index = False)
+		con.close()
+
+def main():
+	processor = ProcessAdjustments()
+	processor.process()
+
+if __name__ == '__main__':
+	main()
