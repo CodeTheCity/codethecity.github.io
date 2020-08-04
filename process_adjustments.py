083ee9725d1e1a6d41524cb70b9e57ab6e980737
Andrew Sage <andrew@asae.co.uk>
2020-08-04 12:23:25 +0100

    Data adjustment layer now applied to original data

--- process_adjustments.py
+++ process_adjustments.py
@@ -18,19 +18,11 @@ class ProcessAdjustments:
 
 		df = pd.DataFrame()
 		
-		dfAdjustments = pd.read_excel(google_sheet_url, header=0, parse_dates=[1], na_values=['(blank)', '(Blank)','Blank','blank', '-',''])
-
-		# Just to see what has been imported
-		#print(dfAdjustments)
+		dfAdjustments = pd.read_excel(google_sheet_url, header=0, parse_dates=[1], na_values=['[Blank]', '(blank)', '(Blank)','Blank','blank', '-',''])
 
 		con = self.db.create_connection()
 		dfOriginal = pd.read_sql_query('SELECT * FROM arrivals_temp ORDER BY date', con, parse_dates=['date'])
 		con.close()
-
-		#print(dfOriginal)
-
-		#print(dfOriginal.dtypes)
-		#print(dfAdjustments.dtypes)
 
 		dfAdjusted = dfOriginal.merge(dfAdjustments[['uuid', 'fishing_port_registration_numbers']], on='uuid')
 
@@ -38,13 +30,18 @@ class ProcessAdjustments:
 
 		dfAdjusted['fishing_port_registration_number'] = dfAdjusted['fishing_port_registration_number'].fillna('')
 
-		print(dfAdjusted)
-		print(dfAdjusted.dtypes)
+		columns = ['vessel','registered_port','master','registered_tonnage','from_port','cargo_transcribed','weather','notes','cargo', 'activity']
+
+		for column in columns:
+			grouped = dfAdjustments.groupby('{}_update'.format(column))
+			for name, group in grouped:
+				dfAdjusted.loc[(dfAdjusted[column].isin([value for value in group[column]])), column] = name		
 
 		# Write to database
 		con = self.db.create_connection()
 		dfAdjusted.to_sql('arrivals', con, if_exists='replace', index = False)
 		con.close()
+
 
 def main():
 	processor = ProcessAdjustments()
